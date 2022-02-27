7d793520db26e3de72570d071ed496962cd648c7
Haley Wakamatsu <japanyoshi777@gmail.com>
2022-02-27 10:15:35 +0900

    high score implementation for arcade

--- ArcadeMenu.gd
+++ ArcadeMenu.gd
@@ -4,12 +4,18 @@ signal ready_finished
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Root.request_bgm("arcade")
-	# TODO: Load high scores when implemented.
 	$Carousel/VBoxContainer/Panel/VBoxContainer/RichTextLabel.bbcode_text = TranslationServer.translate("I18N_MODE_MARATHON_HELP")
 	$Carousel/VBoxContainer/Panel2/VBoxContainer/RichTextLabel.bbcode_text = TranslationServer.translate("I18N_MODE_PETRI_HELP")
 	$Carousel/VBoxContainer/Panel3/VBoxContainer/RichTextLabel.bbcode_text = TranslationServer.translate("I18N_MODE_ONSLAUGHT_HELP")
 	$Carousel/VBoxContainer/Panel4/VBoxContainer/RichTextLabel.bbcode_text = TranslationServer.translate("I18N_MODE_MISSION_HELP")
 	$Carousel/VBoxContainer/Panel5/VBoxContainer/RichTextLabel.bbcode_text = TranslationServer.translate("I18N_MODE_ZEN_HELP")
+
+	var template = "[center]" + TranslationServer.translate("I18N_HIGHSCORE_%s") + "[/center]";
+	$Carousel/VBoxContainer/Panel/VBoxContainer/HS.bbcode_text = template % ("[b]%08d[/b]" % Root.save_dict.arcade.highscores.marathon)
+	$Carousel/VBoxContainer/Panel2/VBoxContainer/HS.bbcode_text = template % ("[b]%08d[/b]" % Root.save_dict.arcade.highscores.petri)
+	$Carousel/VBoxContainer/Panel3/VBoxContainer/HS.bbcode_text = template % ("[b]%08d[/b]" % Root.save_dict.arcade.highscores.onslaught)
+	$Carousel/VBoxContainer/Panel4/VBoxContainer/HS.bbcode_text = template % ("[b]%08d[/b]" % Root.save_dict.arcade.highscores.mission)
+	$Carousel/VBoxContainer/Panel5/VBoxContainer/HS.bbcode_text = template % ("[b]%08d[/b]" % Root.save_dict.arcade.highscores.zen)
 	$Carousel/VBoxContainer/Panel/VBoxContainer/Button.grab_focus()
 	emit_signal("ready_finished")
 
